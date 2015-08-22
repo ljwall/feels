a12bbfc7ad96c0d9861d02d586d7475495b27870
Liam Wall <liam.wall@gmail.com>
2015-08-22 17:05:18 +0100

    Add totals

--- feels.js
+++ feels.js
@@ -4,22 +4,34 @@ angular.module('feels', [ ])
 
 .controller("feelsCtrl", function($scope) {
 	var self = this;
-	this.clear = function() {$scope.message = ""; self.results = []};
+	this.clear = function() {
+		$scope.message = "";
+		self.results = [];
+		self.summary.pos = 0;
+		self.summary.neg = 0;
+	};
+	this.summary = {pos: 0, neg: 0};
 	this.results = [];
     this.check  = function() {
     	self.results = [];
     	var sentence = ($scope.message);
     	var arr = (sentence.split(' '));
     	var sentArr = [];
-    	var score, crass;
+    	var score, crass, sent;
+			self.summary.pos = 0;
+			self.summary.neg = 0;
     	for (var i = 0; i < arr.length; i++) {
-    		score = sentiment(arr[i]).score;
+				sent =  sentiment(arr[i]);
+    		score = sent.score;
     		if (score < 0) {
     			crass = "negative-" + Math.min(5, -score);
     		}
     		else {
     			crass = "positive-" + Math.min(5, score);
     		}
+				console.log(sent.positive);
+				self.summary.pos  += sent.positive.length;
+				self.summary.neg  += sent.negative.length;
     		self.results.push({"class" : crass, "sentence" : arr[i],"score" : score});
     	};
     };
